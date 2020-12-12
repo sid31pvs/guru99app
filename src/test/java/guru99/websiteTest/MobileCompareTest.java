98e46f2836ed8d11a395646b7089b8a601ccbac8
pushpavs31 <siddhantvs311992@gmail.com>
2020-12-12 12:03:56 +0530

    i added new test class

---
+++ src/test/java/guru99/websiteTest/MobileCompareTest.java
@@ -0,0 +1,11 @@
+package guru99.websiteTest;
+
+public class MobileCompareTest {
+	
+	
+	public void compareTwoProduct()
+	{
+		System.out.println("i am comparing two product and checking the price");
+	}
+
+}
